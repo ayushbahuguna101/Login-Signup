5ab6ed78646648a1d1f9b349fb7351822a9031df
Bahuguna <ayush.bahuguna@capgemini.com>
2022-07-27 21:46:08 +0530

    Added Login

---
+++ Login/src/main/java/com/entity/user.java
@@ -0,0 +1,16 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class user {
+	
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	
+	
+
+}
